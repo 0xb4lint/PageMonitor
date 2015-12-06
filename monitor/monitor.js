2206ee683b0eac4c548582ed873dafdb120a6b0f
Szekeres Bálint <szekeres.balint@hps.hu>
2015-12-06 14:33:03 +0100

    working mail

--- monitor/monitor.js
+++ monitor/monitor.js
@@ -8,11 +8,51 @@ var emailConfig	= require(__dirname + '/../email');
 
 var db			= low(__dirname + '/../db.json');
 var queue		= [];
-var transporter	= nodemailer.createTransport( emailConfig );
+var transporter	= nodemailer.createTransport( emailConfig.transporter );
+
+
+
+var createMail = function ( mail, items ) {
+
+	mail.html += '<ul>' + "\n";
+
+		for ( var i in items ) {
+
+			var item = items[ i ];
+
+			mail.html += '<li>' + "\n";
+
+				mail.html += '<b>' + item.name + '</b>' + "\n";
+				mail.html += '<ul>' + "\n";
+
+					if ( item.image )
+						mail.html += '<li><img src="' + item.image + '"></li>' + "\n";
+
+					mail.html += '<li>#' + item.id + '</li>' + "\n";
+					mail.html += '<li><a href="' + item.link + '">' + item.link + '</a></li>' + "\n";
+
+					for ( var j in item ) {
+
+						if ( j === 'name' || j === 'id' || j === 'link' || j === 'image' || !item[ j ] )
+							continue;
+
+						mail.html += '<li>' + j + ': <b>' + item[ j ] + '</b></li>' + "\n";
+					}
+
+				mail.html += '</ul>' + "\n";
+
+			mail.html += '</li>' + "\n";
+		}
+
+	mail.html += '</ul>' + "\n";
+}
 
 
 
 var parse = function ( config, url, email ) {
+
+	console.log( new Date().toLocaleString() );
+	console.log( config.name + ' - ' + email );
 
 	// make the request
 	var response = request('GET', url, { timeout: 15 * 1000 });
@@ -47,8 +87,8 @@ var parse = function ( config, url, email ) {
 	});
 
 	// check items in database
-	var newItems		= 0;
-	var updatedItems	= 0;
+	var newItems		= [];
+	var updatedItems	= [];
 
 	for ( var i in items ) {
 
@@ -58,23 +98,54 @@ var parse = function ( config, url, email ) {
 		if ( !found ) {
 
 			db( config.name + '-' + email ).push( item );
-			newItems++;
+			newItems.push( item );
 
 		} else if ( JSON.stringify( item ) !== JSON.stringify( found ) ) {
 
 			for ( var j in item )
 				found[ j ] = item[ j ];
 
-			updatedItems++;
+			updatedItems.push( item );
 		}
 	}
 
-	console.log( new Date().toLocaleString() );
-	console.log( config.name + ' - ' + email );
 	console.log( 'found', items.length );
-	console.log( 'new', newItems );
-	console.log( 'updated', updatedItems );
+	console.log( 'new', newItems.length );
+	console.log( 'updated', updatedItems.length );
 	console.log('===========');
+
+	if ( email && ( newItems.length || updatedItems.length ) ) {
+
+		var mail = JSON.parse(JSON.stringify( emailConfig.config ));
+
+		mail.subject	+= ' ' + config.name;
+		mail.to			= email;
+		mail.html		= '';
+
+		if ( newItems.length ) {
+
+			mail.html += '<h1>NEW</h1>' + "\n";
+			createMail( mail, newItems );
+		}
+
+		if ( newItems.length && updatedItems.length ) {
+
+			mail.html += '<br>' + "\n";
+			mail.html += '<br>' + "\n";
+		}
+
+		if ( updatedItems.length ) {
+
+			mail.html += '<h1>UPDATED</h1>' + "\n";
+			createMail( mail, updatedItems );
+		}
+
+		transporter.sendMail(mail, function(error, info) {
+
+			if ( error )
+				console.error( error );
+		});
+	}
 };
 
 
